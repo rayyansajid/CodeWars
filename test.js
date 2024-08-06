1a6d37be77657d7d165c8007e3539f0cfcb0bb6e
Rayyan Sajid <114411194+rayyansajid@users.noreply.github.com>
2024-08-06 16:24:10 +0500

    Included Random Tests for Descending Order Problem

--- test.js
+++ test.js
@@ -12,6 +12,20 @@ const DescendingTests = () => {
       assert.strictEqual(descendingOrder(123456789), 987654321);
     });
   });
+  describe("Random tests", () => {  
+  
+    function randint(min, max){
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    for(let i = 0; i < 100; i++){
+      let num = randint(0, 10**randint(1, 10))
+      let expected = +[...""+num].sort().reverse().join``
+      it(`descendingOrder(${num}) should equal ${expected}`, () => {
+        assert.strictEqual(descendingOrder(num), expected);
+      });
+    }
+  })
 }
 
 DescendingTests();
